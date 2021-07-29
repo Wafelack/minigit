d5bf3698795e91e3f54be1bc64ffb897682cd563
Wafelack <wafelack@riseup.net>
2021-07-30 01:50:37 +0200

    refactor: remove useless code

--- main.scm
+++ main.scm
@@ -10,23 +10,4 @@
 
 (define repos (get-repos folder))
 
-#| (let process-commit ((commits (list-commits "../kernel")))
-  (if (not (null? commits))
-    (let* ((commit (car commits))
-           (hash (car commit))
-           (author (car (cdr commit)))
-           (summary (car (cdr (cdr commit))))
-           (time (car (cdr (cdr (cdr commit))))))
-      (begin
-        (newline)
-        (format #t "HASH: ~a" hash)
-        (newline)
-        (format #t "AUTHOR: ~a" author)
-        (newline)
-        (format #t "SUMMARY: '~a'" summary)
-        (newline)
-        (format #t "TIME: ~a" (strftime "%Y-%m-%d %H:%M" (localtime time)))
-        (newline)
-        (process-commit (cdr commits)))))) |#
-
 (run-server (make-request-handler repos folder))
